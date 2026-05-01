c97ba8b97f8d9c172d81b8b6abad7cd0c97323bc
Mael-667 <flamentmael@gmail.com>
2026-05-01 18:26:34 +0200

    feat: sauvegarde des messages via route IPC saveMessage

--- src/Controller/WebsocketController.php
+++ src/Controller/WebsocketController.php
@@ -2,7 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Channel;
+use App\Entity\Message;
+use App\Repository\ChannelRepository;
+use App\Repository\MessageRepository;
 use App\Repository\UserRepository;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -32,9 +38,46 @@ final class WebsocketController extends AbstractController
             ], 400);
         } else {
             return new JsonResponse([
-                "pseudo" => $user->getPseudo()
+                "pseudo" => $user->getPseudo(),
+                "userId" => $user->getId()
             ], 200);
         }
 
+    }
+
+    #[Route('/websocket/saveMessage', name: 'websocket_saveMessage')]
+    public function saveMessage(Request $request, UserRepository $userRep, ChannelRepository $channelRepository, EntityManagerInterface $emi){
+        $secret = $_ENV["IPC_SECRET"];
+        if($secret != $request->headers->get("X-IPC-Secret")){
+            throw new HttpException(403, 'Accès refusé');
+        };
+
+        // TODO: vérifier que l'utilisateur a bien acces au chan où il essaye d'envoyer le message
+        $decodedRequest = json_decode($request->getContent());
+
+        $userId = $decodedRequest->userId;
+        $content = $decodedRequest->message;
+        $channelId = $decodedRequest->channelId;
+        $attachment = $decodedRequest->attachment;
+
+        $message = new Message();
+
+        $channelTarget = $channelRepository->findOneBy(["id" => trim($channelId)]);
+        $user = $userRep->findOneBy(["id" => trim($userId)]);
+
+        if($channelTarget == null){
+            return new JsonResponse([
+                "error" => "Channel inexistant"
+            ], 400);
+        }
+
+        $message->setUser($user);
+        $message->setContent($content);
+        $message->setTimestamp(new DateTime('now'));
+        $message->setAttachment($attachment);
+        $message->setChannel($channelTarget);
+
+        $emi->persist($message);
+        $emi->flush();
     }
 }
--- websocket/ChatWebsocket.php
+++ websocket/ChatWebsocket.php
@@ -8,27 +8,44 @@ require __DIR__."/SymfonyIPC.php";
 // Démarrer la loop après avoir fini de tout initialiser sinon php ne prend plus rien en compte
 $ws = new WebSocketServer();
 
-$si = new SymfonyIPC();
+$symfonyIPC = new SymfonyIPC();
 
-$ws->onMessage(function($msg) use (&$ws, &$si){
+$ws->onMessage(function($msg, $user) use (&$ws, &$symfonyIPC){
     // $ws->broadcastMessage($msg);
-    $details = json_decode($msg);
-    switch ($details->type) {
-        case "authentification":
-            $userDetails = $si->checkCredentials($details->token);
-            echo $userDetails.PHP_EOL;
-            if($userDetails){
-                // associer les infos de l'utilisateur a la connexion
-            } else {
-                // message d'erreur si le token est erroné
-                // rediriger l'utilisateur vers la page de connexion
-            }
-            break;
-        
-        default:
-            // renvoyer un message d'erreur requete invalide
-            break;
+    $message = json_decode($msg);
+
+    if(isset($message->type)){
+        switch ($message->type) {
+            case "authentification":
+                $userDetails = $symfonyIPC->checkCredentials($message->token);
+                if($userDetails){
+                    $user->authenticated = true;
+                    // TODO: générer un token de session
+                    $user->token = $message->token;
+                    $user->id = $userDetails->userId;
+                    // associer les infos de l'utilisateur a la connexion
+                } else {
+                    //todo: message d'erreur si le token est erroné
+                    // rediriger l'utilisateur vers la page de connexion
+                }
+                break;
+            case "message":
+                if(!$user->authenticated) return;
+
+                $content = $message->content;
+                $channel = $message->channel;
+                $attachment = $message->attachment;
+                $symfonyIPC->saveNewMessage($user->id, $content, $attachment, $channel);
+
+                break;
+            default:
+                //todo: renvoyer un message d'erreur requete invalide
+                break;
+        }
+    } else {
+        var_dump($message);
     }
+        
 });
 
 
